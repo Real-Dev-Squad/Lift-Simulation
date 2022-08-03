53d92b2b3606225bfa0b515ff41992762cb0f885
Manish Devrani <manishdevrani777@gmail.com>
2022-08-03 16:11:34 +0530

    refactor code

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,12 @@ const addFloorbtn = document.querySelector('.navbar__add-floor');
 const addLiftBtn = document.querySelector('.navbar__add-lift');
 const allFloors = document.querySelectorAll('.floor');
 const moveLiftUpBtn = document.querySelector('.floor__call-lift-up');
+const moveLiftDownBtn = document.querySelector('.floor__call-lift-down');
 const liftsContainer = document.querySelector('.floor__lifts');
 let lift = document.querySelectorAll('.doors');
 
-let arr = [];
+const destinations = [];
+let numOfLifts = 1;
 const liftsData = [...document.querySelectorAll('.doors')].map((element) => {
   return {
     element,
@@ -15,46 +17,49 @@ const liftsData = [...document.querySelectorAll('.doors')].map((element) => {
   };
 });
 
-const moveLiftUp = (e) => {
+const callLift = (e) => {
   const parentElement = e.target.parentElement.parentElement;
   const floorIndex = parentElement.dataset.floor;
   moveLift(floorIndex);
 };
 
-const findNonBusyLift = () => {
+const findFirstNonBusyLift = () => {
   let nearestLift = {};
   for (let i = 0; i < liftsData.length; i++) {
     if (!liftsData[i].isLiftRunning) {
       nearestLift = { ...liftsData[i], i };
-      break;
+      return nearestLift;
     }
   }
-  return nearestLift;
+  return { ...nearestLift, i: -1 };
+};
+
+const openCloseLift = (index, floorDiff) => {
+  setTimeout(() => {
+    lift[index].classList.add('doors--stop');
+    setTimeout(() => {
+      lift[index].classList.remove('doors--stop');
+      liftsData[index].isLiftRunning = false;
+      if (destinations.length >= 1) {
+        moveLift(Number(destinations.shift()));
+      }
+    }, 5000);
+  }, Math.abs(floorDiff) * 2000 + 1);
 };
 
 const moveLift = (index) => {
-  const { i } = findNonBusyLift();
+  const { i } = findFirstNonBusyLift();
   if (i >= 0) {
     liftsData[i].isLiftRunning = true;
     const floorDiff = liftsData[i].lastFloor - Number(index);
-    console.log(floorDiff);
     lift[i].style.transform = `translateY(${-(Number(index) - 1) * 100}%)`;
     lift[i].style.transition = `transform ${
       Math.abs(floorDiff) * 2000
     }ms ease-out`;
     liftsData[i].lastFloor = Number(index);
-    setTimeout(() => {
-      lift[i].classList.add('doors--stop');
-      setTimeout(() => {
-        lift[i].classList.remove('doors--stop');
-        liftsData[i].isLiftRunning = false;
-        if (arr.length >= 1) {
-          moveLift(Number(arr.shift()));
-        }
-      }, 5000);
-    }, Math.abs(floorDiff) * 2000 + 1);
+    openCloseLift(i, floorDiff);
   } else {
-    arr.push(index);
+    destinations.push(index);
   }
 };
 
@@ -65,7 +70,8 @@ const createFloorWithIndex = (floorIndex) => {
     const floorActionsContainer = document.createElement('div');
     const callLiftUpBtn = document.createElement('button');
     const callLiftDownBtn = document.createElement('button');
-    callLiftUpBtn.addEventListener('click', moveLiftUp);
+    callLiftUpBtn.addEventListener('click', callLift);
+    callLiftDownBtn.addEventListener('click', callLift);
     floorContainer.classList.add('floor');
     floorActionsContainer.classList.add('floor__actions');
     callLiftDownBtn.classList.add('floor__call-lift-down');
@@ -86,7 +92,16 @@ const addNewFloor = () => {
   floors.insertAdjacentElement('afterbegin', createFloor());
 };
 
+const getMaxLiftsAllowed = () => {
+  const liftsContainerWidth = floors.clientWidth;
+  const liftSize = 120 + 32;
+  return Math.floor((liftsContainerWidth - 108) / liftSize);
+};
+
 const addNewLift = () => {
+  if (numOfLifts >= getMaxLiftsAllowed()) {
+    return;
+  }
   const liftEl = document.createElement('div');
   liftEl.classList.add('doors');
   liftsContainer.appendChild(liftEl);
@@ -96,8 +111,10 @@ const addNewLift = () => {
     lastFloor: 1,
     isLiftRunning: false,
   });
+  numOfLifts++;
 };
 
-moveLiftUpBtn.addEventListener('click', moveLiftUp);
+moveLiftUpBtn.addEventListener('click', callLift);
+moveLiftDownBtn.addEventListener('click', callLift);
 addFloorbtn.addEventListener('click', addNewFloor);
 addLiftBtn.addEventListener('click', addNewLift);
