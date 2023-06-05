e971158a2f7529e24807b3648d0329bb5ac74709
mohibkay <mohibk.in@gmail.com>
2023-06-05 12:33:50 +0530

    removed dialog close btn

--- src/js/main.js
+++ src/js/main.js
@@ -5,17 +5,11 @@ const floorSection = document.querySelector('.floor-section');
 const formDialog = document.querySelector('.formDialog');
 const inputForm = document.querySelector('.inputForm');
 const dialogBtn = document.querySelector('.dialogBtn');
-const closeDialog = document.querySelector('.closeDialog');
 const showDialog = document.querySelector('.showDialog');
 const numberOfFloors = document.querySelector('#floors').value;
 const numberOfLifts = document.querySelector('#lifts').value;
 
 formDialog.showModal();
-
-closeDialog.addEventListener('click', () => {
-  formDialog.close('floorOrLiftNotChosen');
-  floorSectionContainer.style.display = 'block';
-});
 
 showDialog.addEventListener('click', () => {
   floorSectionContainer.style.display = 'none';
