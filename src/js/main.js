000292e6ee6f0f122e605cc150bd46af319ec4f1
DevSoni2022 <121338813+DevSoni2022@users.noreply.github.com>
2023-09-23 10:05:52 +0530

    js

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+
+const body1 = document.querySelector("body")
+
+const button1 = document.querySelector(".formButton");
+const input1 = document.querySelector(".input1")
+const input2 = document.querySelector(".input2")
