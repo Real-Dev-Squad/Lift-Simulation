ae2275c3e11044dfe76f979571a07f4d83d5b22e
kushagra <kushagra17800@gmail.com~>
2023-10-26 01:27:28 +0530

    .

--- js/elevators.js
+++ js/elevators.js
@@ -10,6 +10,9 @@ function getQueryParams() {
 // Get query parameters
 const { floors, elevators } = getQueryParams();
 
+// Initialize a variable to keep track of the last floor with a lift image
+let lastLiftFloor = null;
+
 // Display the values
 document.getElementById(
   "floorsDisplay"
@@ -36,16 +39,57 @@ function generateFloor(floorNumber, imageSource) {
   const directionImage = document.createElement("img");
   directionImage.src = "/images/up-down.png"; // Provide the URL to your PNG image
 
+  //experiment code to add lift on a certain floor
+  directionImage.addEventListener("click", (event) => {
+    // Check if the parent div already has a child with a class name 'lift-image'
+    const existingLiftImage = floorImages.querySelector(".lift-image");
+
+    const currId = event.target.parentNode.parentNode.id;
+    // console.log('Parent ID: ' , event.target.parentNode.parentNode.id);
+
+    if (currId != 1 && existingLiftImage) {
+      // If a 'lift-image' child already exists, remove it from the current floor
+      alert("no more then one lift on this floor");
+    } else {
+      if (lastLiftFloor !== null) {
+        // console.log(lastLiftFloor);
+        const convertedNumber = floors - lastLiftFloor + 1;
+        const lastLiftFloorImages = document.querySelector(
+          `.floor:nth-child(${convertedNumber}) .floor-images`
+        );
+        // console.log(lastLiftFloorImages);
+        const existingLiftImage =
+          lastLiftFloorImages.querySelector(".lift-image");
+        if (existingLiftImage) {
+          lastLiftFloorImages.removeChild(existingLiftImage);
+        }
+      }
+      // Update the lastLiftFloor to the current floor
+      lastLiftFloor = floorNumber;
+
+      // If no 'lift-image' child exists, create and append the image to the current floor
+      const floorImage = document.createElement("img");
+      floorImage.src = imageSource; // Provide the URL to your PNG image
+      floorImage.alt = `Floor ${floorNumber}`; // Accessibility text
+      floorImage.className = "lift-image";
+      // Append the image to the current floor
+      floorImages.appendChild(floorImage);
+    }
+  });
+
   // Append the image to the floor
   floorImages.appendChild(directionImage);
 
   // Add an <img> element to the floor
   if (floorNumber == 1) {
-    const floorImage = document.createElement("img");
-    floorImage.src = imageSource; // Provide the URL to your PNG image
-    floorImage.alt = `Floor ${floorNumber}`; // Accessibility text
-    // Append the image to the floor
-    floorImages.appendChild(floorImage);
+    for (let i = 0; i < elevators; i++) {
+      const floorImage = document.createElement("img");
+      floorImage.src = imageSource; // Provide the URL to your PNG image
+      floorImage.alt = `Floor ${floorNumber}`; // Accessibility text
+      // Append the image to the floor
+      floorImage.className = "lift-image";
+      floorImages.appendChild(floorImage);
+    }
   }
   // Append the image to the floor
   floor.appendChild(floorImages);
@@ -59,14 +103,19 @@ function generateFloor(floorNumber, imageSource) {
   // Append the floorname to the floor
   floor.appendChild(floorname);
 
+  //id
+  floor.id = `${floorNumber}`;
+
   // Append the floor to the building container
   buildingContainer.appendChild(floor);
+  lastLiftFloor = floorNumber;
 }
 
 // Generate a specific number of floors (e.g., 5 floors) with images
 
+
 const numberOfFloors = floors;
-for (let i = numberOfFloors; i>=1; i--) {
+for (let i = numberOfFloors; i >= 1; i--) {
   const imageSource = `/images/lift-image.jpg`; // Replace with the actual image URL
   generateFloor(i, imageSource);
 }
