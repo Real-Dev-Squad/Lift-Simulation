7a2eea83d89c29c251aef92e16d3e5f6828b46e2
kushagra <kushagra17800@gmail.com~>
2023-10-28 02:06:30 +0530

    vertical lifts

--- js/elevators.js
+++ js/elevators.js
@@ -47,71 +47,96 @@ function generateFloor(floorNumber, imageSource) {
     const existingLiftImage = floorImages.querySelector(".lift-image");
 
     //getting the current floor id
-    const currFloorId = event.target.parentNode.parentNode.id;
-    // console.log("current floorId: ", event.target.parentNode.parentNode.id);
+    const currFloorId = event.target.parentNode.id;
+    // console.log("current floorId: ", currFloorId);
 
     // Get the div element by its ID (you can use any other method to select the div)
-    var parentDiv = document.getElementById(`building`);
-    // console.log("parent div", parentDiv);
+    var selectedFloorImagesDiv = document.getElementById(`${currFloorId}`).querySelector(".floor-images");
+    // console.log("parent div", selectedFloorImagesDiv.children);
+
+    var swapingColumnId=-1;
+    for(let j=0; j<selectedFloorImagesDiv.children.length; j++) {
+      // console.log("selected column = ",selectedFloorImagesDiv.children[j]);
+      var currFloorImagesDiv = selectedFloorImagesDiv.children[j];
+      var containsImage = currFloorImagesDiv.querySelector("img");
+      if(containsImage==null) {
+        swapingColumnId = selectedFloorImagesDiv.children[j].id;
+        // console.log(swapingColumnId);
+        break;
+      }
+    }
+    if(swapingColumnId==-1) {
+      alert("maxlifts"); return;
+    }
 
     //use this because we are using numbering from bottom to top here.
-    const convertedNumber = floors - currFloorId;
+    var floorLevel = parseInt(currFloorId.match(/\d+/)?.[0]);
+    const convertedNumber = floors - floorLevel;
 
-    // console.log(convertedNumber);
+    // console.log("convertedNumber = ", convertedNumber, floor, currFloorId);
 
     // Loop through child elements to find closest lift that isnt on the current floor
     var foundLiftsOnOtherFloors = 0;
-    for (var i = 0; i < parentDiv.children.length; i++) {
+    var existingLiftImage2;
+    const buildingDiv = document.getElementById("building");
+    for (var i = 0; i < buildingDiv.children.length; i++) {
+      // console.log(floorLevel);
       if(i == convertedNumber) {
         continue;
       }
-      var childElement = parentDiv.children[i];
 
-      // You can now access and manipulate each child element
-      // console.log("Child Element " + i + ": " , childElement);
-
-      // Check if the closest floor div has a child with a class name 'lift-image'
-      const existingLiftImage = childElement.querySelector(".floor-images").querySelector(".lift-image");
-      // console.log(existingLiftImage);
-
-      if (existingLiftImage) {
-        // console.log("Child Element " + i + ": " , childElement);
-          // console.log(childElement.querySelector(".floor-images"));
-          const existingLiftImage = childElement.querySelector(".floor-images").querySelector(".lift-image");
-          childElement.querySelector(".floor-images").removeChild(existingLiftImage);
-          foundLiftsOnOtherFloors=1;
+      // console.log(buildingDiv.children[i].querySelector(".floor-images").children[swapingColumnId]);
+      // console.log(buildingDiv.children[i].querySelector(".floor-images").children.length);
+      const swapImage = buildingDiv.children[i].querySelector(".floor-images").children[swapingColumnId];
+      const containsImage = swapImage.querySelector("img");
+      if(containsImage){
+        // console.log("swapableimage = ",swapImage.querySelector("img"));
+        // console.log("selected floor= ", selectedFloorImagesDiv.children[swapingColumnId]);
+        selectedFloorImagesDiv.children[swapingColumnId].appendChild(swapImage.querySelector("img"));
+        swapImage.removeAttribute("img")
         break;
-      } 
+      }
     }
-    if(foundLiftsOnOtherFloors==1) {
-        //creating the image element and inserting it in clicked floor
-      // If no 'lift-image' child exists, create and append the image to the current floor
-      const floorImage = document.createElement("img");
-      floorImage.src = imageSource; // Provide the URL to your PNG image
-      floorImage.alt = `Floor ${floorNumber}`; // Accessibility text
-      floorImage.className = "lift-image";
-      // Append the image to the current floor
-      floorImages.appendChild(floorImage);
-    }
-    else {
-      alert("maximum lift capacity for this floor is reached");
-    }
-  });
+    // if(foundLiftsOnOtherFloors==1) {
+    //   // Append the image to the current floor
+    //   // console.log("removed = ", existingLiftImage2);
+    //   floorImages.appendChild(existingLiftImage2);
+    // }
+    // else {
+    //   alert("maximum lift capacity for this floor is reached");
+    // }
+  }
+  );
 
   // Append the image to the floor
-  floorImages.appendChild(directionImage);
+  floor.appendChild(directionImage);
 
   // Add an <img> element to the floor
-  if (floorNumber == 1) {
+  if(floorNumber==1) {
     for (let i = 0; i < elevators; i++) {
-      const floorImage = document.createElement("img");
-      floorImage.src = imageSource; // Provide the URL to your PNG image
-      floorImage.alt = `Floor ${floorNumber}`; // Accessibility text
-      // Append the image to the floor
-      floorImage.className = "lift-image";
-      floorImages.appendChild(floorImage);
+      const floorImageDiv = document.createElement("div");
+      floorImageDiv.id = i;
+      floorImageDiv.className = "floorimages-div";
+        const floorImage = document.createElement("img");
+        floorImage.src = imageSource; // Provide the URL to your PNG image
+        floorImage.alt = `Floor ${floorNumber}`; // Accessibility text
+        floorImage.id = `${i+1}`;
+        // Append the image to the floor
+        floorImage.className = "lift-image";
+        floorImageDiv.appendChild(floorImage);
+      floorImages.appendChild(floorImageDiv);
+    }
+  } 
+  else {
+    for (let i = 0; i < elevators; i++) {
+      const floorImageDiv = document.createElement("div");
+      floorImageDiv.className = "floorimages-div";
+      floorImageDiv.id = i;
+      floorImages.appendChild(floorImageDiv);
     }
   }
+    
+    
   // Append the image to the floor
   floor.appendChild(floorImages);
 
@@ -119,13 +144,15 @@ function generateFloor(floorNumber, imageSource) {
   const floorname = document.createElement("div");
 
   // Add content to the floor, such as floor number
-  floorname.textContent = `Floor ${floorNumber}`;
-
+  const floornumber = document.createElement("div");
+  floornumber.className = "floor-number"
+  floornumber.textContent = `Floor ${floorNumber}`;
   // Append the floorname to the floor
   floor.appendChild(floorname);
+  floor.appendChild(floornumber);
 
   //id
-  floor.id = `${floorNumber}`;
+  floor.id = `Floor ${floorNumber}`;
 
   // Append the floor to the building container
   buildingContainer.appendChild(floor);
